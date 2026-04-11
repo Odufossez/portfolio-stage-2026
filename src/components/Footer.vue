f6969e69cbd12cab33fdd54de7afdad272b070db
Oscar Dufossez <oscar.dufossez@edu.univ-fcomte.fr>
2026-04-11 21:05:40 +0200

    Changé les noms dans le footer et juste de l'alignement de code dans NavCard

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,11 @@
     <div class="footer-section acknowledgments">
       <h3>Remerciements à</h3>
       <p class="names">Mes professeurs et l'équipe pédagogique de l'IUT Nord Franche-Comté.</p>
-      <p class="names">Gautier STOLL et Laurence CALZONE, de l'unité 1331 de l'Institut Curie.</p>
-      <p class="names">Juliette REVEILLES, qui m'a permis de trouvé ce stage.</p>
+      <p class="names">Gautier STOLL, Laurence CALZONE, Vincent NOEL et Juliette REVEILLES.</p>
     </div>
 
     <div class="footer-section info">
-      <p>© 2026 - Portfolio Portfolio - Oscar Dufossez</p>
+      <p>© 2026 - Portfolio de stage - Oscar Dufossez</p>
       <a href="https://github.com/odufossez" target="_blank" class="github-btn">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="github-icon"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
         GitHub
--- src/components/NavCard.vue
+++ src/components/NavCard.vue
@@ -4,7 +4,11 @@
       <h3 class="nav-card-title">{{ title }}</h3>
       <div class="nav-card-footer">
         <span>En savoir plus</span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="arrow-icon"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="arrow-icon">
+          <line x1="5" y1="12" x2="19" y2="12"></line>
+          <polyline points="12 5 19 12 12 19"></polyline>
+        </svg>
       </div>
     </div>
   </router-link>
